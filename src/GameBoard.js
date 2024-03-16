d8f9ecd038c4bf78946a9f27ce90b742f4105d5c
Arnold <a03bova@siena.edu>
2024-03-15 23:22:50 -0400

    fixed gameboard :)

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,38 +1,61 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Constants from "expo-constants";
 import { Card } from "./Card";
 
-const cardVals = [
-  ["A", "B", "C", "D"],
-  ["D", "E", "F", "G"],
-  ["A1", "B1", "C1", "D1"],
-  ["D1", "E1", "F1", "G1"],
-];
+// const cardVals = [
+//   ["A", "B", "C", "D"],
+//   ["D", "E", "F", "G"],
+//   ["A1", "B1", "C1", "D1"],
+//   ["D1", "E1", "F1", "G1"],
+// ];
 
 export const GameBoard = ({ onClick, clickVal }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
-  let status = isFlipped;
-  console.log(isFlipped);
+  const [cards, setCards] = useState([
+    [
+      { name: "A", flipped: false },
+      { name: "B", flipped: false },
+      { name: "C", flipped: false },
+      { name: "D", flipped: false },
+    ],
+    [
+      { name: "D", flipped: false },
+      { name: "E", flipped: false },
+      { name: "F", flipped: false },
+      { name: "G", flipped: false },
+    ],
+    [
+      { name: "A1", flipped: false },
+      { name: "B1", flipped: false },
+      { name: "C1", flipped: false },
+      { name: "D1", flipped: false },
+    ],
+    [
+      { name: "D1", flipped: false },
+      { name: "E1", flipped: false },
+      { name: "F1", flipped: false },
+      { name: "G1", flipped: false },
+    ],
+  ]);
+
+  const handleFlip = (i, j) => {
+    let copyOfCards = [...cards];
+    copyOfCards[i][j].flipped = !copyOfCards[i][j].flipped;
+    setCards(copyOfCards);
+  };
   return (
     <View style={styles.container}>
-      {cardVals.map((row, i) => {
-        // console.log(row);
+      {cards.map((row, i) => {
         return (
           <View key={i} style={styles.row}>
             {row.map((cardVal, j) => {
               return (
                 <TouchableOpacity
                   key={j}
-                  onPress={() => {
-                    status = !isFlipped;
-                    setIsFlipped(status);
-                    console.log({ cardVal });
-                    console.log({ status });
-                  }}
-                  value={cardVal}
+                  onPress={() => handleFlip(i, j)}
+                  value={cardVal.name}
                 >
-                  <Card value={cardVal} isFlipped={status} />
+                  <Card value={cardVal.name} isFlipped={cardVal.flipped} />
                 </TouchableOpacity>
               );
             })}
